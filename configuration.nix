3ac19a801e52627af4c010a1a55617ac2d032bda
tfardy <tfardy@4waysolutions.co.uk>
2023-03-21 12:58:37 +0000

    Create configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,101 @@
+{ config, pkgs, ... }:
+
+let
+  HOSTNAME = "<hostname>";
+  TS_KEY = "<tailscale_key>";
+  TS_ROUTE = "<subnet>";
+in
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.users.alice = {
+  #   isNormalUser = true;
+  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  #   packages = with pkgs; [
+  #     firefox
+  #     thunderbird
+  #   ];
+  # };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # environment.systemPackages = with pkgs; [
+  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #   wget
+  # ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.openssh.permitRootLogin = "yes";
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+
+  networking.hostName = "${HOSTNAME}";
+
+  time.timeZone = "Europe/London";
+
+  environment.systemPackages = [ pkgs.tailscale ];
+  services.tailscale.enable = true;
+
+  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+  networking.firewall.enable = false;
+  networking.firewall.checkReversePath = "loose";
+
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+
+    after = [ "network-pre.target" "tailscale.service" ];
+    wants = [ "network-pre.target" "tailscale.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig.Type = "oneshot";
+
+    script = with pkgs; ''
+      sleep 2
+
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        ${tailscale}/bin/tailscale up -authkey ${TS_KEY} --advertise-routes ${TS_ROUTE}
+        exit 0
+      fi
+
+      ${tailscale}/bin/tailscale up -authkey ${TS_KEY} --advertise-routes ${TS_ROUTE}
+    '';
+  };
+
+}
