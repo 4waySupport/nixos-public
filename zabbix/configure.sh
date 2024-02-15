4ff0157a593c86526adec2aacd617ca30d0433cd
Itinfserv <32574362+itinfserv@users.noreply.github.com>
2024-02-15 14:43:01 +0000

    Update configure.sh

--- zabbix/configure.sh
+++ zabbix/configure.sh
@@ -13,6 +13,7 @@ curl $nix_config_url > /etc/nixos/configuration.nix
 
 # Build local configuration
 
+mkdir /docker/zabbix/agent -p
 echo $agentpsk > /docker/zabbix/agent/agentpsk
 echo $agentid > /docker/zabbix/agent/agentid
 
