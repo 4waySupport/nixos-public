4aa0ef2c1dd13ad03b5d5e8cafc58f96c8318164
Itinfserv <32574362+itinfserv@users.noreply.github.com>
2024-02-15 14:30:28 +0000

    Update configure.sh

--- zabbix/configure.sh
+++ zabbix/configure.sh
@@ -5,22 +5,22 @@ nix_config_url="https://raw.githubusercontent.com/4waySupport/nixos-public/main/
 # read -p "Username: " username
 read -p "Hostname: " hostname
 read -p "Tailscale Key: " tskey
-read -p "Do you want to expose a Tailscale Route? [y/n]: " tsroute_enabled
-if [ $tsroute_enabled = y ]
-then
-  read -p "Tailscale route: " tssubnet
-fi
+read -p "Agent registration PSK: " agentpsk
+read -p "Agent registration Identity: " agentid
 
 # Download live config
 curl $nix_config_url > /etc/nixos/configuration.nix
 
 # Build local configuration
+
+echo $agentpsk > /docker/zabbix/agent/agentpsk
+echo $agentid > /docker/zabbix/agent/agentid
+
 cat <<EOF > /etc/nixos/common.nix
 {
   hostname = "$hostname";
   username = "fourway";
   ts_key = "$tskey";
-  tsroute_enabled = "$tsroute_enabled";
-  tssubnet = "$tssubnet";
+  tsroute_enabled = "$tsroute_enabled";  
 }
 EOF
