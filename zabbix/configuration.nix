089d10814c318218df73736944d7c1cceaa56e5d
Itinfserv <32574362+itinfserv@users.noreply.github.com>
2024-03-15 14:30:50 +0000

    Update configuration.nix

--- zabbix/configuration.nix
+++ zabbix/configuration.nix
@@ -58,7 +58,7 @@ let inherit (import /etc/nixos/common.nix) hostname username ts_key tsroute_enab
           environment.ZBX_STARTVMWARECOLLECTORS = "25";
           environment.ZBX_VMWAREFREQUENCY = "60";
           environment.ZBX_VMWAREPERFFREQUENCY = "60";
-          environment.ZBX_VMWARECACHESIZE = "1024M";
+          environment.ZBX_VMWARECACHESIZE = "2G";
           environment.ZBX_VMWARETIMEOUT = "60";
         };
   };
